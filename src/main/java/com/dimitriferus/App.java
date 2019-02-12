247949eb55c7622bd55dd8c3338987da47570991
RyanKolbe <31966635+RyanKolbe@users.noreply.github.com>
2019-02-12 22:33:15 +0200

    Edited by Ryan

--- src/main/java/com/dimitriferus/App.java
+++ src/main/java/com/dimitriferus/App.java
@@ -2,12 +2,10 @@ package com.dimitriferus;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Edited by Ryan");
     }
 }
